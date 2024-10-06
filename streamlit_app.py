51ef81a69449df8bdeb67e66a2e26718203954f6
Tarun Agarwal <49944511+tarunagarwal1981@users.noreply.github.com>
2024-10-06 14:54:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,20 +76,20 @@ def get_vessel_performance_data(engine, vessel_names):
     query_ballast = """
     SELECT VESSEL_NAME, speed_knts, me_power_kw, me_consumption_mt, displacement
     FROM vessel_performance_model_data
-    WHERE vessel_name IN %(vessel_names)s AND load_type = 'Ballast'
+    WHERE vessel_name IN %(vessel_names_list)s AND load_type = 'Ballast'
     """
     query_scantling = """
     SELECT VESSEL_NAME, speed_knts, me_power_kw, me_consumption_mt, displacement
     FROM vessel_performance_model_data
-    WHERE vessel_name IN %(vessel_names)s AND load_type = 'Scantling'
+    WHERE vessel_name IN %(vessel_names_list)s AND load_type = 'Scantling'
     """
     query_design = """
     SELECT VESSEL_NAME, speed_knts, me_power_kw, me_consumption_mt, displacement
     FROM vessel_performance_model_data
-    WHERE vessel_name IN %(vessel_names)s AND load_type = 'Design'
+    WHERE vessel_name IN %(vessel_names_list)s AND load_type = 'Design'
     """
     params = {
-        'vessel_names': tuple([name.upper() for name in vessel_names]) if len(vessel_names) > 1 else tuple([name.upper() for name in vessel_names] + [None])
+        'vessel_names_list': tuple([name.upper() for name in vessel_names])
     }
     df_ballast = pd.read_sql(query_ballast, engine, params=params)
     df_scantling = pd.read_sql(query_scantling, engine, params=params)
